5809fe27433a8ff455c16001ee4fedeccacf3dc5
Su Jang Hao <stevensu@nine-yi.com>
2021-03-16 15:56:29 +0800

    20 Review

---
+++ LeetCode/BL/StackProblem.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using LeetCode.BE;
+
+namespace LeetCode.BL
+{
+    public class StackProblem
+    {
+        /// <summary>
+        /// No.20 Valid Parentheses(Easy)
+        /// </summary>
+        /// <param name="s">string</param>
+        /// <returns>
+        /// Is parentheses valid
+        /// </returns>
+        public bool IsValid(string s)
+        {
+            var stack = new Stack();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(s[i] == '('){
+                    stack.Push('(');
+                }else if(s[i] == '['){
+                    stack.Push('[');
+                }else if(s[i] == '{'){
+                    stack.Push('{');
+                }else if(s[i] == ')'){
+                    if(stack.Count != 0 && (char)stack.Peek() == '(') stack.Pop();
+                    else return false;
+                }else if(s[i] == ']'){
+                    if(stack.Count != 0 && (char)stack.Peek() == '[') stack.Pop();
+                    else return false;
+                }else if(s[i] == '}'){
+                    if(stack.Count != 0 && (char)stack.Peek() == '{') stack.Pop();
+                    else return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
